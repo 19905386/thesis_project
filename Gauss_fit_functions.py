b1306114d8d7c1c9bc3924187f81c9252622b555
19905386 <53447271+19905386@users.noreply.github.com>
2022-03-11 08:23:27 +0200

    Added synthetic profile code

---
+++ Gauss_fit_functions.py
@@ -0,0 +1,466 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+from tqdm import tqdm_notebook as tqdm
+import pickle as pickle
+from support import *
+from features.feature_ts import genX
+from experiment.algorithms.cluster_prep import *
+from array import array
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+import math
+# load pickle module
+import pickle
+from collections import defaultdict
+# Insert the path of modules folder 
+# sys.path.insert(0, 'C:/Users/Jason/thesis_project')
+sys.path.append("../")
+
+from support import *
+from features.feature_ts import genX
+from experiment.algorithms.cluster_prep import *
+
+# Functions that checks for valid peaks
+def range_min3(df, indices_A):
+    dy = [df[i+1]-df[i] for i in range(len(df)-1)]
+
+    if indices_A == 11 or indices_A == 0 or indices_A == 12:
+        return False  
+    else:
+        if dy[indices_A - 1] > 0 and dy[indices_A] < 0:
+            return True    
+        else:
+            return False
+
+# Function that detects peaks in profiles
+def find_peaksMA(X):
+    
+    df = X.copy()
+
+    arrayY = X.to_numpy().copy()
+
+    listYM  = list(map(lambda x : x[0:12],arrayY))
+    listYA  = list(map(lambda x : x[12:],arrayY))
+
+    arrayYM = np.array(listYM)
+    arrayYA = np.array(listYA)
+
+    arrayYM1 = np.array(listYM)
+    arrayYA1 = np.array(listYA)
+
+    indices_M = np.where(arrayYM[0] == np.amax(arrayYM[0]))
+    indices_A = np.where(arrayYA[0] == np.amax(arrayYA[0]))
+
+    while range_min3(arrayYM1[0], indices_M[0][0]) == False:
+        arrayYM[0][np.where(arrayYM[0] == np.amax(arrayYM[0]))] = 0
+        indices_M = np.where(arrayYM[0] == np.amax(arrayYM[0]))
+        # print("arrayYM = {}".format(arrayYM))
+        all_zeros = not np.any(arrayYM[0])
+        if all_zeros == True:
+            return df, False
+        # print("all_zeros = {}".format(all_zeros))
+  
+    while range_min3(arrayYA1[0], indices_A[0][0]) == False:
+        # Find the second largest peak
+        arrayYA[0][np.where(arrayYA[0] == np.amax(arrayYA[0]))] = 0
+        indices_A = np.where(arrayYA[0] == np.amax(arrayYA[0]))
+        all_zeros = not np.any(arrayYA[0])
+        if all_zeros == True:
+            return df, False
+
+    df['0_x'] = indices_M[0] 
+    df['0_y'] = indices_A[0] + 12 
+
+    return df, True
+
+# Function that extracts the time of peaks, and amplitudes of peaks for all the profiles for a single user
+def extractToPs(X):
+  
+    df = X.copy()
+
+    # Do some data conversion magic
+    df.drop(labels = ['date'], inplace = True)
+    df = df.to_frame()
+    df = df.T
+    df.reset_index(inplace = True)
+    df.drop(['index'], axis = 1, inplace = True)
+    df.set_index(['ProfileID'], inplace = True)
+
+    arrayY = df.to_numpy().copy()
+
+    listYM  = list(map(lambda x : x[0:12],arrayY))
+    listYA  = list(map(lambda x : x[12:],arrayY))
+
+    arrayYM = np.array(listYM)
+    arrayYA = np.array(listYA)
+
+    arrayYM1 = np.array(listYM)
+    arrayYA1 = np.array(listYA)
+
+    indices_M = np.where(arrayYM[0] == np.amax(arrayYM[0]))
+    indices_A = np.where(arrayYA[0] == np.amax(arrayYA[0]))
+
+    while range_min3(arrayYM1[0], indices_M[0][0]) == False:
+        arrayYM[0][np.where(arrayYM[0] == np.amax(arrayYM[0]))] = 0
+        indices_M = np.where(arrayYM[0] == np.amax(arrayYM[0]))
+        # print("arrayYM = {}".format(arrayYM))
+        all_zeros = not np.any(arrayYM[0])
+        if all_zeros == True:
+            return False,False, False, False, False
+        # print("all_zeros = {}".format(all_zeros))
+  
+    while range_min3(arrayYA1[0], indices_A[0][0]) == False:
+        # Find the second largest peak
+        arrayYA[0][np.where(arrayYA[0] == np.amax(arrayYA[0]))] = 0
+        indices_A = np.where(arrayYA[0] == np.amax(arrayYA[0]))
+        all_zeros = not np.any(arrayYA[0])
+        if all_zeros == True:
+            return False,False, False, False, False
+
+    mu1 = indices_M[0][0] 
+    mu2 = indices_A[0][0] + 12 
+
+    A1 = df[str(mu1)].values[0]
+    A2 = df[str(mu2)].values[0]
+
+    return A1, A2, mu1, mu2,True
+
+
+# Function that describes the mean profile of a user
+def describe_household(df, id): # Receives the consumption data of single household
+    X = df[df["ProfileID"] == id]
+
+    mean_X = X.groupby(['ProfileID']).mean()
+    X1, check = find_peaksMA(mean_X) # returns information about the profiles peaks and a check flag
+
+    return X1, check
+
+#_____________________________________________________________________________________
+# Functions that determine the sigma values of the gaussians which describe the peaks
+
+def Sigma1(df, increment = 0):
+    # Conditional if statement to check that 
+    p1 = 4 + increment
+
+    i1 = 0
+
+    Xpeak1 = df[str(df['0_x'].values[0])].values[0]
+
+    for c in df:
+        if int(c) != df['0_x'].values[0]:
+            x1 = df[str(df['0_x'].values[0] - int(c))].values[0]
+            temp1 = Xpeak1 - x1
+
+            if temp1 >= 0.0:
+                Xpeak1 = x1
+            else:
+                i1 = df['0_x'].values[0]-int(c) # i returns the index of the lower boundary of range
+                break
+        else:
+            i1 = df['0_x'].values[0]-int(c) # i returns the index of the lower boundary of range
+            break
+
+    range1 = 2*(np.sqrt((df['0_x'].values[0] - i1)**2))
+    
+    sigma1 = range1/p1
+
+    return sigma1, i1
+
+
+def Sigma2(df,increment = 0):
+    # Conditional if statement to check that 
+    p1 = 4 + increment
+
+    i1 = 0
+
+    Xpeak1 = df[str(df['0_x'].values[0])].values[0]# Get the value at the peak
+    
+    for c in df:
+        x1 = df[str(df['0_x'].values[0]+int(c))].values[0]
+        temp1 = Xpeak1 - x1
+        # print("temp1 = Xpeak1 - x1 = {} - {} = {}".format(Xpeak1,x1,temp1))
+        if temp1 >= 0:
+            Xpeak1 = x1
+        else:
+            i1 = df['0_x'].values[0]+int(c) # i returns the index zof the lower boundary of range
+            break
+
+    range1 = 2*(np.sqrt((df['0_x'].values[0] - i1)**2))
+
+    sigma1 = range1/p1
+
+    return sigma1, i1
+
+def Sigma3(df,increment = 0):  
+    # Conditional if statement to check that 
+    p2 = 4 + increment
+
+    # i1 = 0
+    i2 = 0
+
+    Xpeak2 = df[str(df['0_y'].values[0])].values[0]
+
+    for c in df:
+        x2 = df[str(df['0_y'].values[0]-int(c))].values[0]
+        temp2 = Xpeak2 - x2
+        
+        if temp2 >= 0.0:
+            Xpeak2 = x2
+        else:
+            i2 = df['0_y'].values[0]-int(c)+1 # i returns the index of the lower boundary of range
+            break
+
+    range2 = 2*(np.sqrt((df['0_y'].values[0] - i2)**2))
+
+    sigma2 = range2/p2
+
+    return sigma2, i2
+
+
+def Sigma4(df,increment=0):
+    # Conditional if statement to check that 
+    p2 = 4 + increment
+
+    i2 = 0
+
+    Xpeak2 = df[str(df['0_y'].values[0])].values[0]# Get the value at the peak
+    
+    for c in df:
+        try:
+            x2 = df[str(df['0_y'].values[0]+int(c))].values[0] # Get the one after the peak
+            temp2 = Xpeak2 - x2
+            # print("temp2 = Xpeak2 - x2 = {} - {} = {}".format(Xpeak2,x2,temp2))
+            
+            if temp2 >= 0:
+                Xpeak2 = x2
+            else:
+                i2 = df['0_y'].values[0]+int(c) # i returns the index of the lower boundary of range
+                break
+            
+        except KeyError:
+            i2 = df['0_y'].values[0]+int(c)
+            break
+
+    range2 = 2*(np.sqrt((df['0_y'].values[0] - i2)**2))
+    
+    sigma2 = range2/p2
+
+    return sigma2, i2
+#_____________________________________________________________________________________
+
+#Define the Gaussian function
+def gauss(x, H, A, mu, sigma):
+    return H + A * np.exp(-(x - mu) ** 2 / (2 * sigma ** 2))
+
+
+# Define function that approximates straight line
+def straight_line(m,x,b):
+    return m*x+b
+
+# Function that extracts the gauss fit parameters
+def extractFIT(dataframe, houseID):
+    df, check = describe_household(dataframe, houseID)
+    print(df)
+    if check == False:
+        return False,False,False,False,False,False,False,False,False, check
+
+    # Determine the y-offset i.e. minimum value in day
+    H_offset = df.iloc[0,:24].min()
+
+    # Determine the amplitude of gaussian
+    A1 = df[str(df['0_x'].values[0])].values[0] - H_offset # Adjust for offset
+    A2 = df[str(df['0_y'].values[0])].values[0] - H_offset
+
+    # Determine the mean of the gaussian
+    mu1 = df['0_x'].values[0]
+    mu2 = df['0_y'].values[0]
+    # **********************************************************************************************************************************************
+    # Fit the first gaussian
+    sigma1, i1 = Sigma1(df)
+
+    # Calculate error
+    actual = df.iloc[0,i1:df['0_x'].values[0]].to_list()
+    predicted = gauss(np.arange(i1,df['0_x'].values[0],1),H_offset,A1,mu1,sigma1)
+    MSE = mean_squared_error(actual, predicted)
+    RMSE = math.sqrt(MSE)
+
+    tolerance1 = RMSE - RMSE*0.95
+    i = 0.1 # Constant value by which to increase/decrease the range in the initial sigma function
+    t = 0
+    while RMSE > tolerance1:
+        previous_error1 = RMSE
+
+        sigma1, i1= Sigma1(df,increment = i)
+        predicted = gauss(np.arange(i1,df['0_x'].values[0],1),H_offset,A1,mu1,sigma1) # Get the value of the gauss one point above the min peak
+        MSE = mean_squared_error(actual, predicted)
+        RMSE = math.sqrt(MSE)   
+        # conditional for gauss 1
+        if RMSE > previous_error1:
+            i = i + 0.1
+            sigma1, i1 = Sigma1(df,increment = i)
+            predicted = gauss(np.arange(i1,df['0_x'].values[0],1),H_offset,A1,mu1,sigma1) # Get the value of the gauss one point above the min peak
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE) 
+        else:
+            i = i - 0.1
+            sigma1, i1 = Sigma1(df,increment = i)
+            predicted = gauss(np.arange(i1,df['0_x'].values[0],1),H_offset,A1,mu1,sigma1) # Get the value of the gauss one point above the min peak
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE) 
+
+        t = t + 1
+        if t >= 10000:
+            t = 0
+            i = 0
+            tolerance1 = tolerance1 + 0.1
+
+    RMSE1 = RMSE
+    # print("RMSE1 = {}".format(RMSE1))
+    sigma1 = np.sqrt(sigma1**2)
+    # **********************************************************************************************************************************************
+    # Fit the secnd gaussian
+    sigma2, i2= Sigma2(df)
+
+    # Calculate error
+    actual = df.iloc[0,df['0_x'].values[0]:i2].to_list()
+    predicted = gauss(np.arange(df['0_x'].values[0],i2,1),H_offset,A1,mu1,sigma2)
+    MSE = mean_squared_error(actual, predicted)
+    RMSE = math.sqrt(MSE)
+
+    tolerance2 = RMSE - RMSE*0.95
+    # print(tolerance2)
+    i = 0.1 # Constant value by which to increase/decrease the range in the initial sigma function
+    t = 0
+    while RMSE > tolerance2:
+        previous_error2 = RMSE
+        sigma2, i2 = Sigma2(df,increment = i)
+        predicted = gauss(np.arange(df['0_x'].values[0],i2,1),H_offset,A1,mu1,sigma2)
+        MSE = mean_squared_error(actual, predicted)
+        RMSE = math.sqrt(MSE)
+        
+        # Conditional for gauss2
+        if RMSE > previous_error2:
+            i = i + 0.1
+            sigma2, i2 = Sigma2(df,increment = i)
+            predicted = gauss(np.arange(df['0_x'].values[0],i2,1),H_offset,A1,mu1,sigma2)
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE)
+        else:
+            i = i -0.1        
+            sigma2, i2 = Sigma2(df,increment = i)
+            predicted = gauss(np.arange(df['0_x'].values[0],i2,1),H_offset,A1,mu1,sigma2)
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE)
+
+        t = t + 1
+        if t >= 1000:
+            t = 0
+            i = 0
+            tolerance2 = tolerance2 + 0.1
+
+
+    RMSE2 = RMSE
+    # print("RMSE2 = {}".format(RMSE2))
+    sigma2 = np.sqrt(sigma2**2)
+    # **********************************************************************************************************************************************
+    # Fit the third gaussian
+    # Code that finds the value one point above the range index
+    sigma3, i3 = Sigma3(df)
+
+    # Calculate error
+    actual = df.iloc[0,i3:df['0_y'].values[0]].to_list()
+    predicted = gauss(np.arange(i3,df['0_y'].values[0],1),H_offset,A2,mu2,sigma3)
+    MSE = mean_squared_error(actual, predicted)
+    RMSE = math.sqrt(MSE)
+
+    tolerance1 = RMSE - RMSE*0.95
+    i = 0.1 # Constant value by which to increase/decrease the range in the initial sigma function
+    t = 0
+    while RMSE > tolerance1:
+        previous_error3 = RMSE
+        # print(sigma3)
+        # print("RMSE = {}".format(RMSE))
+
+        sigma3, i3 = Sigma3(df,increment = i)
+        predicted = gauss(np.arange(i3,df['0_y'].values[0],1), H_offset, A2, mu2, sigma3)
+        # print('actual = {}'.format(actual))
+        # print('predicted = {}'.format(predicted))
+        MSE = mean_squared_error(actual, predicted)
+        RMSE = math.sqrt(MSE)
+
+        # conditional for gauss 1
+        if RMSE > previous_error3:
+            i = i + 0.1
+            sigma3, i3 = Sigma3(df,increment = i)
+            predicted = gauss(np.arange(i3,df['0_y'].values[0],1),H_offset,A2,mu2,sigma3)
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE)
+        else:
+            i = i - 0.1
+            sigma3, i3 = Sigma3(df,increment = i)
+            predicted = gauss(np.arange(i3,df['0_y'].values[0],1),H_offset,A2,mu2,sigma3)
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE)
+
+        t = t + 1
+        if t >= 10000:
+            t = 0
+            i = 0
+            tolerance1 = tolerance1 + 0.1
+    
+
+    RMSE3 = RMSE
+    # print("RMSE3 = {}".format(RMSE))
+    sigma3 = np.sqrt(sigma3**2)
+    # **********************************************************************************************************************************************
+    # Fit the 4th gaussian
+    sigma4, i4= Sigma4(df)
+
+    actual = df.iloc[0,df['0_y'].values[0]:i4].to_list()
+    predicted = gauss(np.arange(df['0_y'].values[0],i4,1),H_offset,A2,mu2,sigma4)
+    # print(predicted)
+    MSE = mean_squared_error(actual, predicted)
+    RMSE = math.sqrt(MSE)
+
+    tolerance2 = RMSE - RMSE*0.95
+    # print(tolerance2)
+    i = 0.1 # Constant value by which to increase/decrease the range in the initial sigma function
+    t = 0
+    while RMSE > tolerance2:
+        previous_error4 = RMSE
+        # print('sigma4 = {}'.format(sigma4))
+        # print('RMSE = {}'.format(RMSE))
+        sigma4, i4 = Sigma4(df,increment = i)
+        predicted = gauss(np.arange(df['0_y'].values[0],i4,1),H_offset,A2,mu2,sigma4)
+        MSE = mean_squared_error(actual, predicted)
+        RMSE = math.sqrt(MSE)
+        
+        # Conditional for gauss2
+        if RMSE > previous_error4:
+            i = i + 0.1
+            sigma4, i4 = Sigma4(df,increment = i)
+            predicted = gauss(np.arange(df['0_y'].values[0],i4,1),H_offset,A2,mu2,sigma4)
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE)
+
+        else:
+            i = i - 0.1        
+            sigma4, i4 = Sigma4(df,increment = i)
+            predicted = gauss(np.arange(df['0_y'].values[0],i4,1),H_offset,A2,mu2,sigma4)
+            MSE = mean_squared_error(actual, predicted)
+            RMSE = math.sqrt(MSE)
+
+        t = t + 1
+        if t >= 10000:
+            t = 0
+            i = 0
+            tolerance2 = tolerance2 + 0.1
+    # print(tolerance2)
+    RMSE4 = RMSE
+    sigma4 = np.sqrt(sigma4**2)
+    # **********************************************************************************************************************************************
+    # Add the horizontal offset back to the amplitude values
+
+
+    return H_offset, sigma1, sigma2, mu1, A1, sigma3, sigma4, mu2, A2,check
